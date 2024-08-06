d857a97a272d15882c0f256448c34dc2902bd6c5
Aditya <maildevaditya@gmail.com>
2024-08-07 02:19:42 +0530

    completed todo list functionality

--- SimpleTodo/index.js
+++ SimpleTodo/index.js
@@ -24,6 +24,7 @@ function addTodo() {
     Edit.classList.add("edit");
     Edit.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
     Function.appendChild(Edit);
+    Edit.addEventListener("click", () => EditTask(para));
 
     const Complete = document.createElement("span");
     Complete.classList.add("complete");
@@ -35,6 +36,7 @@ function addTodo() {
     Delete.classList.add("delete");
     Delete.innerHTML = `<i class="fa-solid fa-trash"></i>`;
     Function.appendChild(Delete);
+    Delete.addEventListener("click", () => DeleteTask(DataBox));
 
     TodoData.appendChild(DataBox);
   } else {
@@ -46,7 +48,6 @@ function taskComplete(para, DataBox) {
   para.style.textDecoration = "line-through";
 
   setTimeout(() => {
-    DataBox.style.opacity = "0";
     TodoData.removeChild(DataBox);
   }, 250);
 
@@ -65,11 +66,70 @@ function taskComplete(para, DataBox) {
   Restore.classList.add("restore");
   Restore.innerHTML = `<i class="fa-solid fa-arrows-rotate"></i>`;
   Function.appendChild(Restore);
+  Restore.addEventListener("click", () => RestoreTask(parao, CompleteData));
 
   const Delete = document.createElement("span");
   Delete.classList.add("delete");
   Delete.innerHTML = `<i class="fa-solid fa-trash"></i>`;
   Function.appendChild(Delete);
+  Delete.addEventListener("click", () => {
+    CompletedTask.removeChild(CompleteData);
+  });
 
   CompletedTask.appendChild(CompleteData);
+}
+
+function EditTask(para) {
+  para.setAttribute("contenteditable", true);
+  para.focus();
+
+  function saveChanges(event) {
+    if (event.target !== para) {
+      para.setAttribute("contenteditable", false);
+      document.removeEventListener("click", saveChanges);
+    }
+  }
+
+  document.addEventListener("click", saveChanges);
+}
+
+function DeleteTask(DataBox) {
+  TodoData.removeChild(DataBox);
+}
+
+function RestoreTask(parao, CompleteData) {
+  const DataBox = document.createElement("div");
+  DataBox.classList.add("data");
+
+  const para = document.createElement("p");
+  para.innerHTML = parao.innerHTML;
+  DataBox.appendChild(para);
+  DataArray.push(input.value);
+  input.value = "";
+
+  const Function = document.createElement("div");
+  Function.classList.add("function");
+  DataBox.appendChild(Function);
+
+  const Edit = document.createElement("span");
+  Edit.classList.add("edit");
+  Edit.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
+  Function.appendChild(Edit);
+
+  const Complete = document.createElement("span");
+  Complete.classList.add("complete");
+  Complete.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  Function.appendChild(Complete);
+  Complete.addEventListener("click", () => taskComplete(para, DataBox));
+
+  const Delete = document.createElement("span");
+  Delete.classList.add("delete");
+  Delete.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  Function.appendChild(Delete);
+  Delete.addEventListener("click", () => DeleteTask(DataBox));
+
+  TodoData.appendChild(DataBox);
+  setTimeout(() => {
+    CompletedTask.removeChild(CompleteData);
+  }, 250);
 }
